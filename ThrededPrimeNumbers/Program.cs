ffdd82f0334adbb0b995cc2530b0eaf7527450cc
José Torres <github@superjose.com>
2018-10-17 22:25:50 +0200

    Add project files.

---
+++ ThrededPrimeNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThrededPrimeNumbers
+{
+    class Program
+    {
+        static void Main ( string [] args )
+        {
+            Console.WriteLine ( "Hello World!" );
+        }
+    }
+}
