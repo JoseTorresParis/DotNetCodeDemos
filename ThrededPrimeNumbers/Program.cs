87e226038bd3e0931b56fd54d23f4ba969c5eb81
José Torres <contact@josetorres.fr>
2018-10-18 17:45:48 +0200

    init

--- ThrededPrimeNumbers/Program.cs
+++ ThrededPrimeNumbers/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JT.TechCases.Threading;
+using System;
+using System.Diagnostics;
 
 namespace ThrededPrimeNumbers
 {
@@ -6,7 +8,24 @@ namespace ThrededPrimeNumbers
     {
         static void Main ( string [] args )
         {
-            Console.WriteLine ( "Hello World!" );
+            Stopwatch sw = Stopwatch.StartNew ();
+            long valmax = 1000;
+            ThreadPool tp = RangeSplitter.LaunchSplitedTasks ( 1, valmax, 2, ( SubRange sr, long BoundIndex ) =>
+            {
+                bool isprime = true;
+                for ( int a = 2; a <= BoundIndex / 2; a++ )
+                {
+                    if ( BoundIndex % a == 0 )
+                    {
+                        isprime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine ( BoundIndex + " is prime = " + isprime );
+            } );
+            tp.WaitAllFinished ();
+            sw.Stop ();
+            Console.WriteLine ( sw.Elapsed.TotalMilliseconds.ToString ( "0.0###" ) );
         }
     }
 }
